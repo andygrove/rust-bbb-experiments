e554d93384271907b35c2d8724cce14ad54355f1
Andy Grove <andygrove73@gmail.com>
2015-11-14 10:52:48 -0700

    set pin direction

--- blink-led/src/main.rs
+++ blink-led/src/main.rs
@@ -6,6 +6,7 @@ use std::thread::sleep_ms;
 fn main() {
     let my_led = Pin::new(127); // number depends on chip, etc.
     my_led.with_exported(|| {
+        my_led.set_direction(Direction::Out).unwrap();
         loop {
             my_led.set_value(0).unwrap();
             sleep_ms(200);
